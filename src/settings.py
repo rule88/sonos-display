e78256027bbc619b4d7471ffbcec3daedfe91416
Rick Stiphout <rick@nextpertise.nl>
2021-06-22 10:51:44 +0200

    add configurable port

--- src/settings.py
+++ src/settings.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     By default the settings are configured for local development and will not work for production.
     """
     speaker_name: str = 'emtpy'  # client id of github oauth app
+    port: int = 5000
 
 
 settings = Settings()
