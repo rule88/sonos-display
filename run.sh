486c10b5555c1ce5c5f95478a36c3741a2ff7eae
Rick Stiphout <rick@nextpertise.nl>
2021-06-22 11:34:35 +0200

    add run script

---
+++ run.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+export $(cat .env | xargs)
+
+docker build -t sonos-display:latest .
+docker run --restart unless-stopped -d -e PORT=$PORT -e SPEAKER_NAME=$SPEAKER_NAME --network host --name sonos-display sonos-display:latest
