9ec8cbcc906bb76f8166f4ee15dc9689970b6352
Rick Stiphout <rick@nextpertise.nl>
2021-06-22 11:48:53 +0200

    source .env in run script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 # !/bin/bash
 
-export $(cat .env | xargs)
+set -o allexport
+source .env
+set +o allexport
 
 docker build -t sonos-display:latest .
 docker run --restart unless-stopped -d -e PORT=$PORT -e SPEAKER_NAME=$SPEAKER_NAME --network host --name sonos-display sonos-display:latest
