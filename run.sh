ca40cbbe9631b6dd0551e5088ab59ad188f3147a
Rick Stiphout <rick@nextpertise.nl>
2021-06-30 15:28:30 +0200

    add github actions for automaticly building and pushing to dockerhub

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 source .env
 
-docker build -t sonos-display:latest .
-docker run --restart unless-stopped -d -e PORT=$PORT -e SPEAKER_NAME="$SPEAKER_NAME" --network host --name sonos-display sonos-display:latest
+docker run --restart unless-stopped -d -e PORT=$PORT -e SPEAKER_NAME="$SPEAKER_NAME" --network host --name sonos-display rule88/sonos-display:latest
